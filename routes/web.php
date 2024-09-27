61665fa638cd573e61f0e1e0703c24d45093e077
Palistha Prajapati <palistha_mi@yonefu.info>
2024-09-27 18:14:16 +0545

    -add auth middleware in route

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
+Route::middleware(['auth'])->group(function () {
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('/users', App\Http\Controllers\UserController::class);
+});
